59387fb06c592bf6ca3205f09e606c8d4522ec8e
omidsh <omidsh715@gmail.com>
2021-03-09 23:35:45 +0330

    third commit

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Question
 # Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.all()
+
+    return HttpResponse(latest_question_list)
+
+def detail(request, question_id):
+    return HttpResponse("you are watching detail view %s " % question_id )
+
+def results(request, question_id):
+    return HttpResponse("you are watching results view %s " % question_id )
+
+def vote(request, question_id):
+    return HttpResponse("you are watching vote view %s " % question_id)
